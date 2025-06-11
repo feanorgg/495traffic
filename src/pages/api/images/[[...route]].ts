16b8dc61ee63cc66adb5310bf13e231252bf71ed
Ivan Galkin <ivan.galkin5066@gmail.com>
2025-06-11 11:32:40 +0300

    feat: images api

---
+++ src/pages/api/images/[[...route]].ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import fs from "fs";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if(req.method !== "GET") {
+        res.status(405).json({
+            statusCode: 405,
+            message: "This method is not allowed."
+        });
+        return;
+    }
+
+    const { route } = req.query;
+    if(route == undefined) {
+        res.status(400).json({});
+        return;
+    }
+
+    const product_id = route[0];
+    const image_name = route[1];
+
+    const dir = "/home/traffic_images"; 
+    console.log(dir + `/${product_id}/${product_id}_${image_name}`);
+
+    fs.readFile(dir + `/${product_id}/${product_id}_${image_name}`, (error, data) => {
+        if(error) {
+            res.status(404).json({});
+            return;
+        }
+        res.status(200).send(data);
+        return;
+    })
+}
