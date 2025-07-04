0cab0f283a523936c27dbf24d111cac942f27818
Ivan Galkin <ivan.galkin5066@gmail.com>
2025-07-04 16:06:05 +0300

    feat: social links and hide about

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -6,10 +6,10 @@ export default function Footer() {
         <div className={styles.Footer__root}>
             <div className={styles.wrapper}>
                 <div className={styles.links}>
-                    <Link className={`${styles.link} ${styles.left}`} href="/about">
+                    {/*<Link className={`${styles.link} ${styles.left}`} href="/about">
                         About
-                    </Link>
-                    <Link className={styles.link} href="/faq">
+                    </Link>*/}
+                    <Link className={`${styles.link} ${styles.left}`} href="/faq">
                         Faq
                     </Link>
                     <Link className={styles.link} href="/contacts">
@@ -24,13 +24,13 @@ export default function Footer() {
                 </div>
                 <img src="/dngc.png" alt="do_not_get_caught" className={styles.dngc} />
                 <div className={styles.socials}>
-                    <Link className={styles.social} href="/">
+                    <Link className={styles.social} href="https://t.me/donotgetcaught" target="_blank">
                         <img src="/tg.png" alt="tg" />
                     </Link>
-                    <Link className={styles.social} href="/">
+                    <Link className={styles.social} href="https://instagram.com/495traffic" target="_blank">
                         <img src="/is.png" alt="insta" />
                     </Link>
-                    <Link className={styles.social} href="/">
+                    <Link className={styles.social} href="https://vk.com/traffic495" target="_blank">
                         <img src="/vk.png" alt="vk" />
                     </Link>
                 </div>
