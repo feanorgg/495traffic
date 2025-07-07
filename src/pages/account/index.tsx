863a841cd0b6e5f7f11655310c047b988309b4cd
Ivan Galkin <ivan.galkin5066@gmail.com>
2025-07-07 14:47:14 +0300

    account backend (w/o registration yet)

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,34 +1,145 @@
 import Button from "@/components/Button";
 import SizePicker from "@/components/SizePicker";
 import TextInput from "@/components/TextInput";
+import ClientService from "@/services/ClientService";
+import OrderService from "@/services/OrderService";
 import styles from "@/styles/AccountPage.module.scss";
-import { useState } from "react";
+import { Order } from "@/types/Order";
+import User from "@/types/User";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function AccountPage() {
+    const router = useRouter();
+
     const [tabShown, setTabShown] = useState<number>(0);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [user, setUser] = useState<User|null>(null);
+
+    const [firstName, setFirstName] = useState<string>("");
+    const [lastName, setLastName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [phone, setPhone] = useState<string>("");
+
+    const [emailErr, setEmailErr] = useState<string>("");
+    const [phoneErr, setPhoneErr] = useState<string>("");
+
+    const [updateLoading, setUpdateLoading] = useState<boolean>(false);
+
+    const [orders, setOrders] = useState<Array<Order>>([]);
+    const [activeOrders, setActiveOrders] = useState<Array<Order>>([]);
+
+    useEffect(() => {
+        tryAuth();
+    }, []);
+
+    useEffect(() => {
+        if(user) {
+            setFirstName(user.first_name);
+            setLastName(user.last_name);
+            setEmail(user.email);
+            setPhone(user.phone);
+
+            fetchOrders();
+        }
+    }, [user]);
+
+    async function fetchOrders() {
+        const res = await OrderService.getOrdersList();
+        if(res && res.length > 0) {
+            setOrders(res);
+            const _activeOrders = res.filter(o => !["completed", "cancelled"].includes(o.status));
+            setActiveOrders(_activeOrders);
+        }
+    }
+
+    async function tryAuth() {
+        const res = await ClientService.signInWithCookies();
+        if(res && res.email) {
+            setLoading(false);
+            setUser(res);
+            console.log(res);
+        } else {
+            router.push("/account/sign-in");
+        }
+    }
+
+    async function updateUserInfo() {
+        if(updateLoading) return;
+
+        if(email == "") {
+            setEmailErr("Please enter your email");
+            return;
+        } else {
+            setEmailErr("");
+        }
+        if(phone == "") {
+            setPhoneErr("Please enter your phone number");
+            return;
+        } else {
+            setPhoneErr("");
+        }
+
+        setUpdateLoading(true);
+
+        const res = await ClientService.updateUserInfo({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            phone: phone
+        });
+        setUser(res);
+        setUpdateLoading(false);
+    }
+
+    if(loading || user == null) {
+        return(
+            <p>loading</p>
+        );
+    }
 
     return(
         <div className={styles.Account__root}>
             <div className={styles.wrapper}>
                 <div className={styles.left}>
-                    <h2>Welcome, Alexander!</h2>
+                    <h2>Welcome, {user.first_name}!</h2>
                     <p className={styles.c_info_title}>
                         Contact information
                     </p>
                     <div className={styles.info_grid}>
                         <TextInput
-                            value="Alexander"
+                            value={firstName}
+                            onChangeText={setFirstName}
+                            placeholder="First Name"
                         />
                         <TextInput
-                            value="IvanovAlex@gmail.com"
+                            value={email}
+                            onChangeText={setEmail}
+                            placeholder="E-mail"
+                            required
+                            errorMsg={emailErr}
                         />
                         <TextInput
-                            value="Ivanov"
+                            value={lastName}
+                            onChangeText={setLastName}
+                            placeholder="Last Name"
                         />
                         <TextInput
-                            value="+7 (995) 932-32-14"
+                            value={phone}
+                            onChangeText={setPhone}
+                            phone
+                            placeholder="+7 (999) 999-99-99"
+                            required
+                            errorMsg={phoneErr}
                         />
                     </div>
+                    <Button
+                        label={updateLoading ? "Loading..." : "Update Information"}
+                        onClick={updateUserInfo}
+                        loading={updateLoading}
+                        tertiary
+                        style={{width: '100%', marginBottom: '16px'}}
+                    />
                     <Button
                         label="Change Password"
                         tertiary
@@ -47,7 +158,22 @@ export default function AccountPage() {
                     </div>
 
                     <div className={styles.orders_list}>
-                        <OrderTile/>
+                        {tabShown == 0 && activeOrders.length > 0 && activeOrders.map((order, index) => {
+                            return(
+                                <OrderTile
+                                    key={`active_${index}`}
+                                    order={order}
+                                />
+                            );
+                        })}
+                        {tabShown == 1 && orders.length > 0 && orders.map((order, index) => {
+                            return(
+                                <OrderTile
+                                    key={index}
+                                    order={order}
+                                />
+                            );
+                        })}
                     </div>
                 </div>
             </div>
@@ -55,25 +181,36 @@ export default function AccountPage() {
     );
 }
 
-function OrderTile() {
+function OrderTile({order}: {order: Order}) {
     return(
         <div className={styles.OrderTile}>
             <div className={styles.info}>
                 <p className={styles.date}>18.02.2025</p>
-                <p className={styles.code}>№3214</p>
+                <p className={styles.code}>№{order._id}</p>
                 <div className={styles.hor}>
-                    <p className={styles.status}>Status: <span className={styles.delivered}>Delivered</span></p>
-                    <p className={styles.total}>Total amount: 8 575 ₽</p>
+                    <p className={styles.status}>Status: <span className={['cancelled'].includes(order.status) ? styles.cancelled : styles.delivered}>{order.status}</span></p>
+                    <p className={styles.total}>Total amount: {numberWithSpaces(order.total)} ₽</p>
                 </div>
             </div>
 
             <div className={styles.gallery}>
-                <div className={styles.img_c}></div>
-                <div className={styles.img_c}></div>
+                {order.products.slice(0, window.innerWidth >= 1440 ? 2 : 1).map((product, index) => {
+                    return(
+                        <div className={styles.img_c} key={index}>
+                            <img src={product.images[0].min} alt={product.name} />
+                        </div>
+                    );
+                })}
+                {order.products.length > 3 &&
                 <div className={styles.more}>
-                    <p>+ 4</p>
+                    <p>+ {order.products.length - 2}</p>
                 </div>
+                }
             </div>
         </div>
     );
+}
+
+function numberWithSpaces(x: number) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
--- src/pages/account/sign-in.tsx
+++ src/pages/account/sign-in.tsx
@@ -1,10 +1,57 @@
 import Button from "@/components/Button";
 import Checkbox from "@/components/Checkbox";
 import TextInput from "@/components/TextInput";
+import ClientService from "@/services/ClientService";
 import styles from "@/styles/SignUpPage.module.scss";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function SignInPage() {
+    const router = useRouter();
+
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const [emailErr, setEmailErr] = useState<string>("");
+    const [passwordErr, setPasswordErr] = useState<string>("");
+
+    useEffect(() => {
+        async function checkAuth() {
+            const res = await ClientService.signInWithCookies();
+            if(res && res.email) {
+                router.push("/account");
+            }
+        }
+        checkAuth();
+    });
+
+    async function trySignIn() {
+        if(email == "") {
+            setEmailErr("Please enter your email");
+            return;
+        } else {setEmailErr("");}
+        if(password == "") {
+            setPasswordErr("Please enter your password");
+            return;
+        } else {setPasswordErr("");}
+        if(password.length < 8) {
+            setPasswordErr("Password must be at least 8 characters long");
+            return;
+        } else {setPasswordErr("");}
+
+        const res = await ClientService.signInWithEmailAndPassword({
+            email: email,
+            password: password
+        });
+
+        if(res && res.email) {
+            router.push("/account");
+        } else {
+            setPasswordErr("Invalid email or password");
+        }
+    }
+
     return(
         <div className={styles.SignUp__root}>
             <div className={`${styles.frame} ${styles.signin_frame}`}>
@@ -14,14 +61,22 @@ export default function SignInPage() {
                     </p>
                     <TextInput
                         placeholder="E-mail"
+                        value={email}
+                        onChangeText={setEmail}
+                        errorMsg={emailErr}
                     />
                     <TextInput
                         placeholder="Password"
+                        secure
+                        value={password}
+                        onChangeText={setPassword}
+                        errorMsg={passwordErr}
                     />
                     <Button
                         label="Log In"
                         tertiary
                         style={{width: '100%'}}
+                        onClick={() => trySignIn()}
                     />
                     <div className={styles.hor}>
                         <p className={styles.txt}>
