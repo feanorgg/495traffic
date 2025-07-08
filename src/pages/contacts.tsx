f55a830ce14ca804d75523a606c53d7563e45aa7
Ivan Galkin <ivan.galkin5066@gmail.com>
2025-07-08 17:35:21 +0300

    mobile ui for /contacts

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -3,8 +3,51 @@ import styles from "@/styles/ContactsPage.module.scss";
 import TextInput from "@/components/TextInput";
 import Checkbox from "@/components/Checkbox";
 import Button from "@/components/Button";
+import { useState } from "react";
 
 export default function ContactsPage() {
+    const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [message, setMessage] = useState<string>("");
+    const [dataAgreement, setDataAgreement] = useState<boolean>(false);
+
+    const [nameErr, setNameErr] = useState<string>("");
+    const [emailErr, setEmailErr] = useState<string>("");
+    const [messageErr, setMessageErr] = useState<string>("");
+    const [dataAgreementErr, setDataAgreementErr] = useState<boolean>(false);
+
+    const [formLoading, setFormLoading] = useState<boolean>(false);
+    const [sent, setSent] = useState<boolean>(false);
+
+    async function trySendForm() {
+        if(name == "") {
+            setNameErr("Please enter your name");
+            return;
+        } else {setNameErr("");}
+        if(email == "") {
+            setEmailErr("Please enter your email");
+            return;
+        } else {setEmailErr("");}
+        if(!String(email).toLowerCase().match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )) {
+            setEmailErr("Incorrect email, please try again");
+            return;
+        } else {setEmailErr("");}
+        if(message == "") {
+            setMessageErr("Please enter your message");
+            return;
+        } else {setMessageErr("");}
+        if(!dataAgreement) {
+            setDataAgreementErr(true);
+            return;
+        } else {setDataAgreementErr(false);}
+
+        setFormLoading(true);
+
+        // send 
+    }
+
     return(
         <>
         <Head>
@@ -41,20 +84,36 @@ export default function ContactsPage() {
                             <p className={styles.title}>Any other questions? Write to us and we will contact you as soon as possible.</p>
                             <TextInput
                                 placeholder="Name"
+                                value={name}
+                                onChangeText={setName}
+                                errorMsg={nameErr}
                             />
                             <TextInput
                                 placeholder="E-mail"
+                                value={email}
+                                onChangeText={setEmail}
+                                errorMsg={emailErr}
                             />
                             <TextInput
                                 placeholder="Message"
                                 multiline
+                                value={message}
+                                onChangeText={setMessage}
+                                errorMsg={messageErr}
                             />
-                            <Checkbox>
+                            <Checkbox
+                                checked={dataAgreement}
+                                onChange={setDataAgreement}
+                                error={dataAgreementErr}
+                            >
                                 <p>I agree with the <a href="/privacy" target="_blank">personal data processing policy</a></p>
                             </Checkbox>
                             <Button
-                                label="Send"
+                                label={formLoading ? "Sending..." : "Send"}
                                 secondary
+                                loading={formLoading}
+                                disabled={sent}
+                                onClick={() => trySendForm()}
                             />
                         </div>
                     </div>
