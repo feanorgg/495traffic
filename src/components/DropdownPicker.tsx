bc166663d2f879a1668559f3d6f7d2bb794d5ee0
Ivan Galkin <ivan.galkin5066@gmail.com>
2025-07-15 01:44:29 +0300

    last cdn fix

--- src/components/DropdownPicker.tsx
+++ src/components/DropdownPicker.tsx
@@ -34,7 +34,7 @@ export default function DropdownPicker({
         <div className={styles.DropdownPicker} ref={boxRef}>
             <div className={styles.container} onClick={() => setState(!state)}>
                 <p className={`${styles.label} ${state ? styles.active : ''}`}>{value == "" ? placeholder : value}</p>
-                <img alt="chevron" src={state ? '/arrow-up.png' : '/arrow-down.png'} />
+                <img alt="chevron" src={state ? 'https://cdn2.495traffic.com/arrow-up.png' : 'https://cdn2.495traffic.com/arrow-down.png'} />
             </div>
             {state &&
             <div className={styles.items_frame}>
